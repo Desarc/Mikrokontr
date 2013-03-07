f2205baa86e0ff6fe424a13bcd86c0225d071f64
Sandor Zeestraten <zeestrat@dmlab09.idi.ntnu.no>
2013-03-07 16:53:08 +0100

    almost music

--- ex2/oeving2.c
+++ ex2/oeving2.c
@@ -6,12 +6,29 @@
 
 #include "oeving2.h"
 
+#define max_sample_size 1024
+#define sound_length 5000
+#define Fs 46875
+
+volatile int sample_size;
 volatile int LED_VECTOR = 0x0;
+volatile int sound[max_sample_size];
+volatile int sound2[107];
+volatile int counter;
+volatile int counter2;
+volatile int playingSound = 0;
 
 int main (int argc, char *argv[]) {
-  initHardware();
-  while(1);
-  return 0;
+	int i;
+	for (i = 0; i < max_sample_size; i++) {
+		sound[i] = 0;
+	}
+	for (i = 0; i < 107; i++) {
+		sound2[i] = 0;
+	}
+	initHardware();
+ 	while(1);
+  	return 0;
 }
 
 
@@ -57,7 +74,7 @@ void initLeds(void) {
 
 void initAudio(void) {
 	register_interrupt( abdac_isr, AVR32_ABDAC_IRQ/32, AVR32_ABDAC_IRQ%32, ABDAC_INT_LEVEL);
-	sm->gcctrl[6] = 0x4;  //using OSC0
+	pm->gcctrl[6] = 0x5;  //using OSC1
 	piob->PDR.p20 = 1;
 	piob->PDR.p21 = 1;
 	piob->ASR.p20 = 1;
@@ -72,29 +89,35 @@ void button_isr(void) {
 	clearLeds();
 	int buttons = pioc->pdsr;
 	if (buttons == BUTTON7) {
-		playSound(TEST);
+		playSound(SOUND7);
 		LED_VECTOR = LED7;
 	}
 	else if (buttons == BUTTON6) {
-		dac->sdr = 0;
+		playSound(SOUND6);
 		LED_VECTOR = LED6;
 	}
 	else if (buttons == BUTTON5) {
+		playSound(SOUND5);
 		LED_VECTOR = LED5;
 	}
 	else if (buttons == BUTTON4) {
+		playSound(SOUND4);
 		LED_VECTOR = LED4;
 	}
 	else if (buttons == BUTTON3) {
+		playSound(SOUND3);
 		LED_VECTOR = LED3;
 	}
 	else if (buttons == BUTTON2) {
+		playSound(SOUND2);
 		LED_VECTOR = LED2;
 	}
 	else if (buttons == BUTTON1) {
+		playSound(SOUND1);
 		LED_VECTOR = LED1;
 	}
 	else if (buttons == BUTTON0) {
+		playSound(SOUND0);
 		LED_VECTOR = LED0;
 	}
 	setLeds();
@@ -103,31 +126,75 @@ void button_isr(void) {
 }
 
 void playSound(int code) {
-	//this is probably completely wrong.
-	int sample_size = 10000;
-	int sound[sample_size];
+	float f;
+	if (code == SOUND7) {
+		f = A4;
+		//sample_size = 107;
+	}
+	else if (code == SOUND6) {
+		f = A4;
+		//sample_size = 108;
+	}
+	else if (code == SOUND5) {
+		f = A4;
+		//sample_size = 109;
+	}
+	else if (code == SOUND4) {
+		f = D9;
+		//sample_size = 100;
+	}
+	else if (code == SOUND3) {
+		f = E9;
+		//sample_size = 100;
+	}
+	else if (code == SOUND2) {
+		f = F9;
+		//sample_size = 100;
+	}
+	else if (code == SOUND1) {
+		f = G9;
+		//sample_size = 100;
+	}
+	else if (code == SOUND0) {
+		f = A4;
+		//sample_size = 100;
+	}
+	sample_size = ceil(Fs/f);
+	if (sample_size > max_sample_size) {
+		sample_size = max_sample_size;
+	}
+	
+	int A = 1500;
 	int i;
 	for (i = 0; i < sample_size; i++) {
-		sound[i] = (int)floor(sin(1000*(2*M_PI)*i/44100));
-	}
-	int j;
-	for (j = 0; j < sample_size; j++) {
-		dac->sdr = sound[i];
+		sound[i] = (int)floor(A*sin(f*(2*M_PI)*i/Fs));
 	}
 
-
-}
-
-void miniWait(void) {
-	int n = 0x0;
-	while (n < 0xfff) {
-		n++;
-	}	
-	return;
+	//for (i = 0; i < 107; i++) {
+	//	sound2[i] = (int)floor(A*sin(f*(2*M_PI)*i/46875));
+	//}
+	counter = 0;
+	counter2 = 0;
+	playingSound = 1;
+	dac->sdr = sound[counter];
+	counter++;
 }
 
 void abdac_isr(void) {
-
+	//piob->isr;
+	if (playingSound == 0) {
+		return;
+	}
+	if (counter2 == sound_length) {
+		playingSound = 0;
+		return;
+	}
+	if (counter == sample_size) {
+		counter = 0;
+		counter2++;
+	}
+	dac->sdr = sound[counter];
+	counter++;
 	return;
 }
 
--- ex2/oeving2.h
+++ ex2/oeving2.h
@@ -18,6 +18,23 @@
 #define ABDAC_INT_LEVEL 0
 #define BUTTONS_INT_LEVEL 0
 
+#define A4 440.00
+#define B4 493.88
+#define C5 523.25
+#define D5 587.33
+#define E5 659.26
+#define F5 698.46
+#define G5 783.99
+
+const float A8 = 4*A4;
+const float B8 = 4*B4;
+const float C9 = 4*C5;
+const float D9 = 4*D5;
+const float E9 = 4*E5;
+const float F9 = 4*F5;
+const float G9 = 4*G5;
+//const float A9 = 5*A4;
+
 const int SET_ALL = 0xff;
 const int BUTTON7 = 0xffff3f7f;
 const int BUTTON6 = 0xffff3fbf;
@@ -37,15 +54,18 @@ const int LED2 = 0x04;
 const int LED1 = 0x02;
 const int LED0 = 0x01;
 
-const int TEST = 0;
-
-const int SAMPLE_RATE = 4000;
-const int SAMPLE_SIZE = 8;
-const int channels = 4;
+const int SOUND0 = 0;
+const int SOUND1 = 1;
+const int SOUND2 = 2;
+const int SOUND3 = 3;
+const int SOUND4 = 4;
+const int SOUND5 = 5;
+const int SOUND6 = 6;
+const int SOUND7 = 7;
 
 volatile avr32_pio_t *piob = &AVR32_PIOB;
 volatile avr32_pio_t *pioc = &AVR32_PIOC;
-volatile avr32_pm_t *sm = &AVR32_PM;
+volatile avr32_pm_t *pm = &AVR32_PM;
 volatile avr32_abdac_t *dac = &AVR32_ABDAC;
 
 
@@ -61,6 +81,5 @@ static void button_isr(void);
 static void abdac_isr(void);
 static void debounce(void);
 static void playSound(int code);
-static void miniWait(void);
 
 #endif
